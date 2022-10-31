b60bf60768db834c30912100d2fe341312208305
Ray Chiang <ray.chiang@amazingtalker.com>
2022-10-31 19:59:45 +0800

    test: update test case

--- impl_test.go
+++ impl_test.go
@@ -1,59 +1,75 @@
 package localcache
 
 import (
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNew(t *testing.T) {
+func TestSet(t *testing.T) {
+	// mock time.Now()
+	timeNow = func() time.Time {
+		return time.Unix(1629446406, 0)
+	}
+
+	expect := &cache{
+		es: map[string]*entry{
+			"a": {
+				val:    "123",
+				expiry: timeNow().Add(30 * time.Second),
+			},
+		},
+	}
+
 	c := New()
+	c.Set("a", "123")
 
-	got := len(c.es)
-
-	assert.Equal(t, got, 0, "New cache size should be zero.")
+	assert.Equal(t, reflect.DeepEqual(c, expect), true, "Should be equal")
 }
 
 func TestGet(t *testing.T) {
-	c := New()
+	c := &cache{
+		es: map[string]*entry{
+			"a": {
+				val:    123,
+				expiry: timeNow().Add(30 * time.Second),
+			},
+		},
+	}
 
-	c.Set("a", 123)
-	got, ok := c.Get("a")
-	size := len(c.es)
+	val, err := c.Get("a")
 
-	assert.Nil(t, ok, "Should be nil")
-	assert.Equal(t, size, 1, "Cache size should be one")
-	assert.Equal(t, got, 123, "Should be equal")
+	assert.Nil(t, err, "Should be nil")
+	assert.Equal(t, val, 123, "Should be equal")
 }
 
-func TestSet(t *testing.T) {
+func TestGet_KeyNotExist(t *testing.T) {
 	c := New()
 
-	c.Set("a", 123)
-	c.Set("a", "456")
-	got, ok := c.Get("a")
+	val, err := c.Get("a")
 
-	assert.Equal(t, got, "456", "Should be equal")
-	assert.Nil(t, ok, "Should be nil")
+	assert.Nil(t, val)
+	assert.Equal(t, err, ErrKeyNotExist, "Key doesn't exist in cache")
 }
 
-func TestKeyNotExist(t *testing.T) {
-	c := New()
+func TestGet_KeyExpiry(t *testing.T) {
+	c := &cache{
+		es: map[string]*entry{
+			"a": {
+				val:    123,
+				expiry: timeNow().Add(30 * time.Second),
+			},
+		},
+	}
 
-	got, ok := c.Get("a")
+	// mock time.Time.Before()
+	timeBefore = func(_ time.Time, _ time.Time) bool {
+		return true
+	}
+	val, err := c.Get("a")
 
-	assert.Nil(t, got)
-	assert.Equal(t, ok, ErrKeyNotExist, "Key doesn't exist in cache")
-}
-
-func TestKeyExpiry(t *testing.T) {
-	c := New()
-
-	c.Set("a", 123)
-	time.Sleep(30 * time.Second)
-	got, ok := c.Get("a")
-
-	assert.Nil(t, got)
-	assert.Equal(t, ok, ErrKeyExpiry, "Key expiry")
+	assert.Nil(t, val)
+	assert.Equal(t, err, ErrKeyExpiry, "Key expiry")
 }
