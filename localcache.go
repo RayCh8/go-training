e98961d2b15da763ae2a9f7a8948ec683d251fa3
Ray Chiang <ray.chiang@amazingtalker.com>
2022-11-01 12:28:22 +0800

    feat: move error variable to impl.go

---
+++ localcache.go
@@ -0,0 +1,17 @@
+package localcache
+
+import (
+	"time"
+)
+
+var (
+	defaultTTL = time.Second * 30
+)
+
+// Cache is used to record data that user frequently fetch
+type Cache interface {
+	// Get is used to fetch value by key.
+	Get(key string) (interface{}, error)
+	// Set is used to set value by key
+	Set(key string, val interface{})
+}
